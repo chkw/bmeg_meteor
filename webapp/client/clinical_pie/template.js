e613bc79e2918a1f005d43bb7699cf007700f7f5
chrisw <chrisw@soe.ucsc.edu>
2016-08-17 16:51:42 -0700

    working on clinical pies

--- webapp/client/clinical_pie/template.js
+++ webapp/client/clinical_pie/template.js
@@ -3,7 +3,44 @@ import {
 }
 from 'meteor/session';
 
-Template.clinicalPieTemplate.events({});
+Template.clinicalPieTemplate.events({
+  'click button#submitButtonClinicalVarNames': function(event, instance) {
+    console.log("event", "click button#submitButtonClinicalVarNames");
+
+    var divElem = document.getElementById("clinicalPiesDiv");
+
+    // start throbber
+    document.getElementById("throbberImg")
+      .style.display = "inline";
+
+    // callback for getting data
+    var buildPies = function(error, result) {
+      console.log("result", result);
+
+      if (result.success) {
+        console.log("got some data. let's build some pies!");
+        var data = result.data.data;
+
+      } else { // remove child elements of divElem
+        while (divElem.firstChild) {
+          divElem.removeChild(divElem.firstChild);
+        }
+        divElem.innerHTML = 'no data';
+        alert("request failed!");
+      }
+
+      // stop throbber
+      document.getElementById("throbberImg")
+        .style.display = "none";
+    };
+
+    // get data via the Meteor.method
+    // Meteor.call("get_hard_coded_data", selectedSigs, buildObsDeckWithData);
+    Meteor.call("post_get_event_data", [], [], [
+      "submittedTumorSite"
+    ], buildPies);
+  }
+});
 
 Template.clinicalPieTemplate.helpers({});
 
@@ -14,38 +51,5 @@ Template.clinicalPieTemplate.onCreated(function() {});
 
 Template.clinicalPieTemplate.onRendered(function() {
   console.log("Template.clinicalPieTemplate.rendered");
-
-  var divElem = document.getElementById("obsDeckDiv");
-
-  // start throbber
-  document.getElementById("throbberImg")
-    .style.display = "inline";
-
-  // callback for getting data
-  var buildPies = function(error, result) {
-    console.log("result", result);
-
-    if (result.success) {
-      console.log("got some data. let's build some pies!");
-      var data = result.data.data;
-
-    } else { // remove child elements of divElem
-      while (divElem.firstChild) {
-        divElem.removeChild(divElem.firstChild);
-      }
-      divElem.innerHTML = 'no data';
-      alert("request failed!");
-    }
-
-    // stop throbber
-    document.getElementById("throbberImg")
-      .style.display = "none";
-  };
-
-  // get data via the Meteor.method
-  // Meteor.call("get_hard_coded_data", selectedSigs, buildObsDeckWithData);
-  Meteor.call("post_get_event_data", [], [], [
-    "submittedTumorSite"
-  ], buildPies);
 
 });
