34fe1da38a7782a12a4047a707e14b50fbc3826f
chrisw <chrisw@soe.ucsc.edu>
2016-08-24 16:37:18 -0700

    add button to get to clinical pie charts

--- webapp/client/welcome/template.js
+++ webapp/client/welcome/template.js
@@ -1,14 +1,19 @@
-import { Session
-}from'meteor/session';
+import {
+    Session
+} from 'meteor/session';
 
 Template.welcomeTemplate.events({
-    'click button#gene_set' : function(event, instance) {
+    'click button#gene_set': function(event, instance) {
         Session.set("use_case", 1);
         FlowRouter.go("gene_sets");
     },
-    'click button#gene_set_2' : function(event, instance) {
+    'click button#gene_set_2': function(event, instance) {
         Session.set("use_case", 2);
         FlowRouter.go("gene_sets");
+    },
+    'click button#browse_clinical_pies': function(event, instance) {
+        Session.set("use_case", null);
+        FlowRouter.go("clinicalPie");
     }
 });
 
